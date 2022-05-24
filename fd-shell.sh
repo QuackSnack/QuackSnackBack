65d6cc34b41d812e2ef4623e9e804d072101200b
Gregory <gregory.hue135@gmail.com>
2022-05-24 21:50:17 +0200

    Updated: 2022-05-24 21:50:17

--- fd-shell.sh
+++ fd-shell.sh
@@ -28,13 +28,17 @@ function fd-back() {
 
 # Pulls the changes on every projects
 function fd-pull() {
+    printf  "${CYAN}pulling FoodDistributionBack${NC}\n"
     cd $HOME/dev/FoodDistributionBack && git status && git fetch && git pull
+    printf  "${CYAN}pulling FoodDistributionFront${NC}\n"
     cd $HOME/dev/FoodDistributionFront && git status && git fetch && git pull
 }
 
 # Push the changes on every projects
 function fd-push() {
+    printf  "${CYAN}pushing to FoodDistributionBack${NC}\n"
     cd $HOME/dev/FoodDistributionBack && git status && git add . && git commit -m "Updated: `date +'%Y-%m-%d %H:%M:%S'`" && git push
+    printf  "${CYAN}puhsing to FoodDistributionFront${NC}\n"
     cd $HOME/dev/FoodDistributionFront && git status && git add . && git commit -m "Updated: `date +'%Y-%m-%d %H:%M:%S'`" && git push
 }
 
