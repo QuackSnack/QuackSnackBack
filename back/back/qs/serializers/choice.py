405140100d29f84ff3c65650b65daf6457a93535
Gregory <gregory.hue135@gmail.com>
2022-09-22 00:01:04 +0200

    Push: 22-09-2022 00:01:04

--- back/back/qs/serializers/choice.py
+++ back/back/qs/serializers/choice.py
@@ -1,8 +1,10 @@
 from rest_framework import serializers
+from back.qs.serializers.article import ArticleSerializer
 from back.qs.models.choice import Choice
 
 
 class ChoiceSerializer(serializers.ModelSerializer):
+  possibilities = ArticleSerializer(many=True)
 
   class Meta:
     model = Choice
